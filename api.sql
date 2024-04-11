3a836c23ae959bf519ed5f4d44c6b8d90368d383
Gustavo Romero <pasanteit@inventa.com.sv>
2024-04-11 06:44:20 -0600

    Base de API events

---
+++ api.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE eventmanagement
+GO
+USE eventmanagement
+GO
+CREATE TABLE events(
+	eventId int IDENTITY (1,1) PRIMARY KEY CLUSTERED NOT NULL,
+	eventTitle nvarchar (100) NOT NULL,
+	eventDescription nvarchar (1500) NOT NULL,
+	startDate date NOT NULL,
+	endDate date NOT NULL,
+	avenue nvarchar (200) NOT NULL,
+	maxMembers int NOT NULL
+)
+GO
